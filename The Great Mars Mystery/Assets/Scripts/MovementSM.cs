c0bd233db89b9abfd8ebd88f5d4aa25fb4dfda75
Garrison <garrison.jabs@gmail.com>
2022-07-15 17:33:13 -0600

    Finite state machine for controlling movement

---
+++ The Great Mars Mystery/Assets/Scripts/MovementSM.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovementSM : StateMachine
+{
+    [HideInInspector]
+    public Idle idleState;
+    [HideInInspector]
+    public Moving movingState;
+
+    [Header("Player Rigidbody")]
+    public Rigidbody2D rb;
+
+    [Header("Player Transform")]
+    public Transform tf;
+    
+    [Header("Camera Transform")]
+    public Transform ctf;
+
+    public float speed = 4f;
+    
+
+    private void Awake()
+    {
+        idleState = new Idle(this);
+        movingState = new Moving(this);
+      
+
+    }
+    
+    protected override BaseState GetInitialState()
+    {
+        return idleState;
+    }
+
+}
---
+++ The Great Mars Mystery/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    BaseState currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = GetInitialState();
+        if (currentState != null)
+        {
+            currentState.enter();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentState != null)
+        {
+            currentState.updateLogic();
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (currentState != null)
+        {
+            currentState.updatePhysics();
+        }
+    }
+
+    public void ChangeState(BaseState newState)
+    {
+        currentState.exit();
+
+        currentState = newState;
+
+        currentState.enter();
+    }
+
+    protected virtual BaseState GetInitialState()
+    {
+        return null;
+    }
+
+    //Prints the current state on the screen for debugging purposes
+    private void OnGUI()
+    {
+        string content = currentState != null ? currentState.name : "(no current state)";
+        GUILayout.Label($"<color='white'><size=40>{content}</size></color>");
+    }
+}
+
