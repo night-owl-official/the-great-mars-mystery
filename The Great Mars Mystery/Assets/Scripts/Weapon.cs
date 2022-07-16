0cd067891ecf6a9dd92b5ff18fdf52c1c985afbc
Ahmed Mohammed Abdelaziz Hasan <ahmed.abdelaziz.hasan92@gmail.com>
2022-07-16 17:40:12 +0200

    Basic raycast shooting, include the weapon script in the player gameobject. A child shooting point gameobject to the player and attach it to the script

---
+++ The Great Mars Mystery/Assets/Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform shootingPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit2D hitInfo= Physics2D.Raycast(shootingPoint.position, shootingPoint.right);
+
+        if(hitInfo)
+        {
+            Debug.Log(hitInfo.transform.name);
+        }
+    }
+}
