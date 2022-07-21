e1900eb1dd23c92b68ccccfa95eea965f059dae1
Daniel Grieco <daniel_grieco@programmer.net>
2022-07-21 21:32:48 +0200

    Refactor CarMovement script by fixing up some comments, remove extra variables and organizing the car's actions into functions.

--- The Great Mars Mystery/Assets/Scripts/CarMovement.cs
+++ The Great Mars Mystery/Assets/Scripts/CarMovement.cs
@@ -1,58 +1,59 @@
 ﻿using UnityEngine;
 /*The sprites pivot point will dictate how the car appears to steer 
- * remember to set the point near the back of the vehicle */
+ * it's recommended to set it near the back of the vehicle */
 
 
 // Make sure the Car movement has access to Rigidbody and Collider
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
-public class CarMovement : MonoBehaviour
-{
+public class CarMovement : MonoBehaviour {
     #region Methods
     // Called once at the start
-    void Start()
-    {
+    private void Start() {
         m_rb = GetComponent<Rigidbody2D>();
     }
 
     // Called once every frame
-    void Update()
-    {
+    private void Update() {
         // Retrieve the user's input with a value between -1 and 1
-        m_horizontal = Input.GetAxisRaw("Horizontal");
-        m_vertical = Input.GetAxisRaw("Vertical");
+        // Some input smoothing is preferred for vehicles
+
+        // Invert the sideways input to match with the turn direction
+        // e.g. right arrow = turn right
+        m_hInput = Input.GetAxis("Horizontal") * -1;
+        m_vInput = Input.GetAxis("Vertical");
     }
 
     // Called once every Physics update
-    void FixedUpdate()
-    {
-        /*
-        
-        we assign the speed amount to the up and down controls
-        we calculate the dot product  
+    private void FixedUpdate() {
+        ApplyEngineForce();
+        ApplySteering();
+    }
 
-         */
-        //we assign the direction of rotation to the left and right controls
-        //this is the rotation around the vertical axis(pivot point) or YAW of the vehicle
-        m_yaw = -m_horizontal;
-        //we assign the speed control to the up and down controls
-        m_speed = m_vertical * m_acceleration;
-        //The dot product of the velocity and the relitive direction of up is calculated,
-        //if the two vectors are the same direction the product is positive, and if they are oposite
-        //the value is negative.
-        //Mathf.Sign then gives us a positive 1 or -1 relative to the calculated product
-        //this allows us to avoid using if statement to select direction of rotation
-        m_direction = Mathf.Sign(Vector2.Dot(m_rb.velocity, m_rb.GetRelativeVector(Vector2.up)));
-        //set the amount of rotation to be added when a force is applied
-        m_rb.rotation += m_yaw * m_yawRate * m_rb.velocity.magnitude * m_direction;
+    /// <summary>
+    /// Constructs the speed vector and uses it as the engine force
+    /// propelling the car forward.
+    /// </summary>
+    private void ApplyEngineForce() {
+        // Speed vector allows the car to move forward and backwards,
+        // alongside the car's local y axis (forward in 2D), based on the user input
+        m_speed = m_acceleration * m_vInput * transform.up;
+        m_rb.AddForce(m_speed);
+    }
 
-        //adds the force to the vehicle in the forward or backward direction 
-        //related to the up or down arrow
-        m_rb.AddRelativeForce(Vector2.up * m_speed);
+    /// <summary>
+    /// Figures out the direction of the rotation and applies it to the car.
+    /// </summary>
+    private void ApplySteering() {
+        // The dot product allows us to figure out which way the car is going to turn
+        // by calculating the angle between the velocity and the forward direction (up in 2D)
+        // Mathf.Sign then gives us a positive 1 or -1 relative to the calculated product
+        // which allows us to avoid using if statement to select direction of rotation
+        m_turnDirection = Mathf.Sign(Vector2.Dot(m_rb.velocity, m_rb.GetRelativeVector(Vector2.up)));
 
-        //adds a small force in the left and right direction to compensate for some vehicle drift
-        //on button release
-        m_rb.AddRelativeForce(-Vector2.right * m_rb.velocity.magnitude * m_yaw / 2);
+        // Apply a rotation to the object based on its velocity and turn speed.
+        // User input is also taken into account alongside the turn direction.
+        m_rb.rotation += m_hInput * m_turnSpeed * m_rb.velocity.magnitude * m_turnDirection;
     }
     #endregion
 
@@ -60,14 +61,13 @@ public class CarMovement : MonoBehaviour
     [SerializeField]
     private float m_acceleration = 5f;
     [SerializeField]
-    private float m_yawRate = 5f;
+    private float m_turnSpeed = 5f;
 
     private Rigidbody2D m_rb;
 
-    private float m_horizontal;
-    private float m_vertical;
-    private float m_yaw;
-    private float m_speed;
-    private float m_direction;
+    private float m_hInput;
+    private float m_vInput;
+    private Vector2 m_speed;
+    private float m_turnDirection;
     #endregion
 }
