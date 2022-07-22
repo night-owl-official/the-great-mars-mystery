a53de6fe00f06799683659b95c1aaebbb80123d6
Daniel Grieco <daniel_grieco@programmer.net>
2022-07-22 03:55:27 +0200

    Create a PlayerShooting script and assign it to the Player prefab.

---
+++ The Great Mars Mystery/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    #region Methods
+    #endregion
+
+    #region Member variables
+    [SerializeField]
+    private Bullet m_bulletPrefab;
+
+    [SerializeField]
+    [Tooltip("The offset from the player where the bullet will spawn")]
+    private Vector2 m_bulletSpawnPoint;
+    #endregion
+}
