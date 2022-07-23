63ea0e09360e404f44466cfb11986c505576dc50
Ahmed Mohammed Abdelaziz Hasan <ahmed.abdelaziz.hasan92@gmail.com>
2022-07-23 12:58:28 +0200

    Rotating to character to mouse position. Firing to mouse position. Trail for the bullets.

---
+++ The Great Mars Mystery/Assets/Scripts/RotateWithMouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithMouse : MonoBehaviour
+{
+    private Camera mainCam;
+    private Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This calculated the angle to rotate the character by in order to follow the mouse
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mousePos- transform.position;
+        float rotZ = Mathf.Atan2(rotation.y,rotation.x)* Mathf.Rad2Deg-90f;
+        transform.rotation=Quaternion.Euler(0,0,rotZ);
+    }
+}
---
+++ The Great Mars Mystery/Assets/Scripts/Weapon.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // The point in space where the ray is cast
+    public Transform shootingPoint;
+    public LineRenderer bulletEffect;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine (Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        //Records information about the object it hit
+        RaycastHit2D hit = Physics2D.Raycast(shootingPoint.position,shootingPoint.up);
+
+        //If anything is hit with the ray and it is not null
+        if(hit)
+        {
+            Debug.Log(hit.transform.name);
+
+            bulletEffect.SetPosition(0,shootingPoint.position);
+            bulletEffect.SetPosition(1,hit.point);
+        }
+        else
+        {
+            bulletEffect.SetPosition(0,shootingPoint.position);
+            bulletEffect.SetPosition(1,shootingPoint.position + shootingPoint.up * 100);
+        }
+
+        bulletEffect.enabled=true;
+
+        yield return new WaitForSeconds(0.02f);
+
+        bulletEffect.enabled=false;
+    }
+}
