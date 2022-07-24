8995d4233f51f974c14395d8011537be3c3454de
Ari Gold <33474861+arigold22@users.noreply.github.com>
2022-07-24 17:45:09 +0530

    NPC Movement and Player Detection
    Enemies/NPC can rotate from one position to another. Another script called DetectPlayer can be added if you want that enemy to detect the player when they get to a certain distance.

---
+++ The Great Mars Mystery/Assets/Scripts/AI/AIPatrol.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPatrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float walkSpeed;
+    public bool isPatrolling;
+    public Rigidbody2D rb;
+    public GameObject[] targets;
+    public int currentTarget;
+
+    [HideInInspector]
+    public Vector3 direction;
+
+    [HideInInspector]
+    public float angle;
+
+    public float rotationAdjustment;
+
+    void Start()
+    {
+        isPatrolling = true;
+        currentTarget = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPatrolling)
+        {
+            Patrol();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isPatrolling)
+        {
+            rb.MovePosition(rb.transform.position + (direction * walkSpeed * Time.deltaTime));
+            rb.MoveRotation(angle);
+        }
+    }
+
+    void Patrol()
+    {
+
+        direction = targets[currentTarget].transform.position - rb.transform.position;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - rotationAdjustment;
+        direction.Normalize();
+        float dist = Vector3.Distance(targets[currentTarget].transform.position, rb.transform.position);
+        if (dist < 0.1)
+        {
+            Debug.Log(currentTarget);
+            currentTarget = currentTarget + 1;
+            if (currentTarget == targets.Length)
+            {
+                currentTarget = 0;
+            }
+        }
+    }
+        
+}
+
+
