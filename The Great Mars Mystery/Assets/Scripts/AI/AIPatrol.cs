592f43aef05cc56935f9fa62bc3e632a79fe6f90
Daniel Grieco <daniel_grieco@programmer.net>
2022-07-28 17:39:50 +0200

    Refactor the AIPatrol script to match coding style.

--- The Great Mars Mystery/Assets/Scripts/AI/AIPatrol.cs
+++ The Great Mars Mystery/Assets/Scripts/AI/AIPatrol.cs
@@ -1,68 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class AIPatrol : MonoBehaviour
-{
-    // Start is called before the first frame update
-
-    public float walkSpeed;
-    public bool isPatrolling;
-    public Rigidbody2D rb;
-    public GameObject[] targets;
-    public int currentTarget;
-
-    [HideInInspector]
-    public Vector3 direction;
-
-    [HideInInspector]
-    public float angle;
-
-    public float rotationAdjustment;
-
-    void Start()
-    {
-        isPatrolling = true;
-        currentTarget = 0;
-
+// Make sure the Player movement has access to Rigidbody and Collider
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class AIPatrol : MonoBehaviour {
+    #region Methods
+    // Called once at the start of the game
+    private void Start() {
+        m_rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (isPatrolling)
-        {
-            Patrol();
+    // Called once per frame
+    private void Update() {
+        // Go to the next waypoint when the current one was visited
+        if (m_isPatrolling)
+            SwitchWaypoint();
+    }
+
+    // Called once every physics update
+    private void FixedUpdate() {
+        if (m_isPatrolling) {
+            // Get the movement direction from the current position to the next waypoint
+            Vector2 direction =
+                m_waypoints[m_currentWaypoint] - m_rb.position;
+            // Calculate the facing rotation angle
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            m_rb.MovePosition(m_rb.position +
+                (m_walkSpeed * Time.deltaTime * direction.normalized));
+            m_rb.MoveRotation(angle);
         }
     }
 
-    private void FixedUpdate()
-    {
-        if (isPatrolling)
-        {
-            rb.MovePosition(rb.transform.position + (direction * walkSpeed * Time.deltaTime));
-            rb.MoveRotation(angle);
-        }
-    }
+    /// <summary>
+    /// Switches from the current waypoint to the next when the current waypoint is reached.
+    /// </summary>
+    private void SwitchWaypoint() {
+        float dist =
+            Vector2.Distance(m_waypoints[m_currentWaypoint], m_rb.position);
 
-    void Patrol()
-    {
-
-        direction = targets[currentTarget].transform.position - rb.transform.position;
-        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - rotationAdjustment;
-        direction.Normalize();
-        float dist = Vector3.Distance(targets[currentTarget].transform.position, rb.transform.position);
-        if (dist < 0.1)
-        {
-            Debug.Log(currentTarget);
-            currentTarget = currentTarget + 1;
-            if (currentTarget == targets.Length)
-            {
-                currentTarget = 0;
-            }
-        }
+        // Switch to the next waypoint after the current one has been explored
+        if (dist < m_stoppingDistance)
+            // Move onto the next waypoint in the list and loop back around
+            // to the first waypoint when the last is reached
+            m_currentWaypoint = (m_currentWaypoint + 1) % m_waypoints.Length;
     }
-        
+    #endregion
+
+    #region Memeber variables
+    [SerializeField]
+    private float m_walkSpeed = 1f;
+
+    [SerializeField]
+    [Tooltip("How far this AI will stop from the waypoint")]
+    private float m_stoppingDistance = 0.2f;
+
+    [SerializeField]
+    private Vector2[] m_waypoints;
+
+    private int m_currentWaypoint = 0;
+    private bool m_isPatrolling = true;
+    private Rigidbody2D m_rb;
+    #endregion
 }
-
-
--- The Great Mars Mystery/Assets/Scripts/AI/DetectPlayer.cs
+++ The Great Mars Mystery/Assets/Scripts/AI/DetectPlayer.cs
@@ -1,73 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class DetectPlayer : MonoBehaviour
-{
-    public Rigidbody2D rb;
-    public GameObject player;
-    public float detectionRadius;
-    public float escapeRadius;
-    public bool isChasing;
-    public float rotationAdjustment;
-    public float maintainRelativeDist;
+//public class DetectPlayer : MonoBehaviour
+//{
+//    public Rigidbody2D rb;
+//    public GameObject player;
+//    public float detectionRadius;
+//    public float escapeRadius;
+//    public bool isChasing;
+//    public float rotationAdjustment;
+//    public float maintainRelativeDist;
 
-    [HideInInspector]
-    public Vector3 direction;
+//    [HideInInspector]
+//    public Vector3 direction;
 
-    [HideInInspector]
-    public float angle;
+//    [HideInInspector]
+//    public float angle;
 
-    [HideInInspector]
-    public float dist;
+//    [HideInInspector]
+//    public float dist;
 
-    AIPatrol aiPatrol;
-    // Start is called before the first frame update
-    void Start()
-    {
-        isChasing = false;
+//    AIPatrol aiPatrol;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        isChasing = false;
 
-        aiPatrol = this.GetComponent<AIPatrol>();
+//        aiPatrol = this.GetComponent<AIPatrol>();
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        dist = Vector3.Distance(player.transform.position, rb.transform.position);
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        dist = Vector3.Distance(player.transform.position, rb.transform.position);
 
-        //Debug.Log(isChasing);
-        if (dist < detectionRadius)
-        {
-            isChasing = true;
-            aiPatrol.isPatrolling = false;
-        }
+//        //Debug.Log(isChasing);
+//        if (dist < detectionRadius)
+//        {
+//            isChasing = true;
+//            aiPatrol.m_isPatrolling = false;
+//        }
 
-        if (dist > escapeRadius)
-        {
-            isChasing = false;
-            aiPatrol.isPatrolling = true;
-        }
+//        if (dist > escapeRadius)
+//        {
+//            isChasing = false;
+//            aiPatrol.m_isPatrolling = true;
+//        }
 
-        if (isChasing)
-        {
-            direction = player.transform.position - rb.transform.position;
-            angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - rotationAdjustment;
-            direction.Normalize();
-        }
-    }
+//        if (isChasing)
+//        {
+//            direction = player.transform.position - rb.transform.position;
+//            angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - rotationAdjustment;
+//            direction.Normalize();
+//        }
+//    }
 
-    private void FixedUpdate()
-    {
-        if (isChasing) {
+//    private void FixedUpdate()
+//    {
+//        if (isChasing) {
             
-            if (dist > maintainRelativeDist)
-            {
-                rb.MovePosition(rb.transform.position + (direction * aiPatrol.walkSpeed * Time.deltaTime));
-            }
+//            if (dist > maintainRelativeDist)
+//            {
+//                rb.MovePosition(rb.transform.position + (direction * aiPatrol.m_walkSpeed * Time.deltaTime));
+//            }
             
-            rb.MoveRotation(angle);
-        }
-    }
-}
+//            rb.MoveRotation(angle);
+//        }
+//    }
+//}
 
