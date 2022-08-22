ffcbc7508f7a612556170172a86fe97e3d696418
Daniel Grieco <daniel_grieco@programmer.net>
2022-08-22 17:40:47 +0200

    Refactor Dialogue and DialogueTrigger scripts

--- The Great Mars Mystery/Assets/Scripts/Dialogue.cs
+++ The Great Mars Mystery/Assets/Scripts/Dialogue.cs
@@ -3,59 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Dialogue : MonoBehaviour
-{
+public class Dialogue : MonoBehaviour {
     #region Methods
-    private void Awake()
-    {
+    private void Awake() {
         ToggleIndicator(false);
         ToggleWindow(false);
     }
 
-    private void ToggleWindow(bool show)
-    {
+    private void ToggleWindow(bool show) {
         window.SetActive(show);
     }
 
-    public void ToggleIndicator(bool show)
-    {
+    public void ToggleIndicator(bool show) {
         indicator.SetActive(show);
     }
-
-    //Start Dialogue
-    public void StartDialogue()
-    {
-        if(started)
-        {
+    
+    public void StartDialogue() {
+        if (started)
             return;
-        }
 
-        //Boolean to indicate that we have started
         started = true;
-        //Show the window
         ToggleWindow(true);
-        //hide the indicator
         ToggleIndicator(false);
+
         //start with first dialogue
         GetDialogue(0);
     }
 
-    private void GetDialogue(int i)
-    {
-        //start index at zero
+    private void GetDialogue(int i) {
         index = i;
-        //reset the character index
         charIndex = 0;
-        //clear the dialogue component text
         dialogueText.text = string.Empty;
-        //start writing
+        
         StartCoroutine(Writing());
     }
 
-    //End Dialogue
-    public void EndDialogue()
-    {
-        //Hide the window
+    public void EndDialogue() {
         ToggleWindow(false);
 
         //Reset the dialogue to start
@@ -66,77 +49,65 @@ public class Dialogue : MonoBehaviour
 
     //Writing logic
     //Writes the text like a typewriter one letter at a time
-    IEnumerator Writing()
-    {
-        //Wait x seconds
+    private IEnumerator Writing() {
         yield return new WaitForSeconds(writingSpeed);
 
         string currentDialogue = dialogues[index];
-        //Write the character
-        dialogueText.text += currentDialogue[charIndex];
-        //increase the character index
-        charIndex++;
+        
+        dialogueText.text += currentDialogue[charIndex++];
+        
         //Make sure you have reached the end of the sentence
-        if(charIndex < currentDialogue.Length)
-        {
-            //Wait x seconds
+        if (charIndex < currentDialogue.Length) {
             yield return new WaitForSeconds(writingSpeed);
             //restart the same process
             StartCoroutine(Writing());
-        }
-        else
-        {
+        } else {
             //End this sentence and wait for the next one
             waitForNext = true;
         }
     }
 
-    private void Update()
-    {
-        if(!started)
-        {
+    private void Update() {
+        if (!started)
             return;
-        }
 
-        if(waitForNext && Input.GetButtonDown("Interact"))
-        {
+        if (waitForNext && Input.GetButtonDown("Interact")) {
             waitForNext = false;
             index++;
 
             //Check if we are in the scope of dialogues List
-            if (index < dialogues.Count)
-            {
+            if (index < dialogues.Count) {
                 //If so fetch the next dialogue
                 GetDialogue(index);
-            }
-            else
-            {
+            } else {
                 //If not End dialogue
                 ToggleIndicator(true);
                 EndDialogue();
-            }  
+            }
         }
     }
     #endregion
 
     #region Member variables
-    //Window
-    public GameObject window;
-    //Indicator
-    public GameObject indicator;
-    //Text component
-    public TMP_Text dialogueText;
-    //Dialogues list
-    public List<string> dialogues;
-    //Writing speed
-    public float writingSpeed;
+    [SerializeField]
+    private GameObject window;
+
+    [SerializeField]
+    private GameObject indicator;
+
+    [SerializeField]
+    private TMP_Text dialogueText;
+
+    [SerializeField]
+    private List<string> dialogues;
+
+    [SerializeField]
+    private float writingSpeed;
+
     //Selected Dialogue index
     private int index;
-    //Character index
     private int charIndex;
-    //Started boolean
     private bool started;
-    //Wait for next boolean
     private bool waitForNext;
     #endregion
 }
--- The Great Mars Mystery/Assets/Scripts/DialogueTrigger.cs
+++ The Great Mars Mystery/Assets/Scripts/DialogueTrigger.cs
@@ -1,43 +1,35 @@
 ﻿using UnityEngine;
 
 
-public class DialogueTrigger : MonoBehaviour
-{
+public class DialogueTrigger : MonoBehaviour {
     #region Methods
-    //Detect trigger with player
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //If we triggered the player enable player detected and show indicator
-        if(collision.tag == "Player")
-        {
+    private void Start() {
+        dialogueScript = GetComponentInChildren<Dialogue>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
             playerDetected = true;
             dialogueScript.ToggleIndicator(playerDetected);
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        //If we triggered the player enable player detected and show indicator
-        if (collision.tag == "Player")
-        {
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
             playerDetected = false;
             dialogueScript.ToggleIndicator(playerDetected);
             dialogueScript.EndDialogue();
         }
     }
 
-    //While detected if we interact start the dialogue
-    private void Update()
-    {
-        if(playerDetected && Input.GetButtonDown("Interact"))
-        {
+    private void Update() {
+        if (playerDetected && Input.GetButtonDown("Interact"))
             dialogueScript.StartDialogue();
-        }
     }
     #endregion
 
     #region Member variables
-    public Dialogue dialogueScript;
+    private Dialogue dialogueScript;
     private bool playerDetected;
     #endregion
 }
