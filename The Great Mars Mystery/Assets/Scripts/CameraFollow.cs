b52a0b116ad6810c2912a1dbd5bf4093bb5214b7
Daniel Grieco <daniel_grieco@programmer.net>
2022-08-22 18:39:07 +0200

    Add smoothing to CameraFollow script and refactor it.

--- The Great Mars Mystery/Assets/Scripts/CameraFollow.cs
+++ The Great Mars Mystery/Assets/Scripts/CameraFollow.cs
@@ -1,38 +1,40 @@
 ﻿using UnityEngine;
 
-/*Attaches to the main camera and updates it transform to be the same as the players
+/*Attaches to the main camera and updates its transform to be the same as the players
  will track the car instead if the player gameObject is deactivated*/
-public class CameraFollow : MonoBehaviour
-{
+public class CameraFollow : MonoBehaviour {
     #region Methods
-    private void Start()
-    {
-        m_playerTransform = player.transform;
-        m_carTransform = car.transform;
+    private void Start() {
+        playerTransform = GameObject.FindWithTag("Player")?.transform;
+        carTransform = GameObject.FindWithTag("Car")?.transform;
     }
 
-    private void FixedUpdate()
-    {
-        if (player != null)
-        {
-            if (player.activeSelf)
-            {
-                //updates the camera transform to the players changed position
-                this.transform.position = new Vector3(m_playerTransform.position.x, m_playerTransform.position.y, this.transform.position.z);
-            }
-            else if (car !=null)
-            {
-                //updates the camera transform to the cars changed position only if the player gameobject has been deactivated
-                this.transform.position = new Vector3(m_carTransform.position.x, m_carTransform.position.y, this.transform.position.z);
-            }
-        }
+    private void LateUpdate() {
+        if (!playerTransform)
+            return;
+
+        if (playerTransform.gameObject.activeSelf)
+            LerpCurrentPositionToTargetPosition(playerTransform.position);
+        else if (carTransform)
+            LerpCurrentPositionToTargetPosition(carTransform.position);
+    }
+
+    private void LerpCurrentPositionToTargetPosition(Vector3 targetPosition) {
+        transform.position =
+            new Vector3(
+                Mathf.Lerp(transform.position.x, targetPosition.x, cameraSmoothing * Time.deltaTime),
+                Mathf.Lerp(transform.position.y, targetPosition.y, cameraSmoothing * Time.deltaTime),
+                transform.position.z
+            );
     }
     #endregion
 
     #region Member variables
-    public GameObject player;
-    public GameObject car;
-    private Transform m_playerTransform;
-    private Transform m_carTransform;
+    [SerializeField]
+    [Range(0.1f, 10f)]
+    private float cameraSmoothing = 1.5f;
+
+    private Transform playerTransform;
+    private Transform carTransform;
     #endregion
 }
