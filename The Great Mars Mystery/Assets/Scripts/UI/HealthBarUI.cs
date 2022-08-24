db1bd1db45bf35d7f3d574992f9397b0e2be73c8
Ari Gold <33474861+arigold22@users.noreply.github.com>
2022-08-24 15:56:26 +0530

    Added Health Bar for Player and Enemy

---
+++ The Great Mars Mystery/Assets/Scripts/UI/HealthBarUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+    public Slider slider;
+
+    // Start is called before the first frame update
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+}
