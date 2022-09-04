3a713de9ca584a123edc12f21a425264e459b392
Ahmed Mohammed Abdelaziz Hasan <ahmed.abdelaziz.hasan92@gmail.com>
2022-09-04 18:11:02 +0200

    Added enemy health

--- The Great Mars Mystery/Assets/Scripts/EnemyHealth.cs
+++ The Great Mars Mystery/Assets/Scripts/EnemyHealth.cs
@@ -1,12 +1,22 @@
-﻿public class EnemyHealth : Health {
+﻿using UnityEngine.UI;
+public class EnemyHealth : Health {
     #region Methods
     // Start is called before the first frame update
     protected override void Start() {
         // Set the current health to max
         m_currentHP = m_maxHP;
+        enemyHPBar=gameObject.GetComponentInChildren<Slider>();
+        enemyHPBar.maxValue=m_maxHP;
+        enemyHPBar.value=m_currentHP;
+    }
+
+    public override void DeductHPs(float amount) {
+        base.DeductHPs(amount);
+        enemyHPBar.value=m_currentHP;
     }
     #endregion
 
     #region Member variables
+    public Slider enemyHPBar;
     #endregion
 }
