ea4f644e13340e12e0f947e9fc669d9cc9183bdc
Daniel Grieco <daniel_grieco@programmer.net>
2022-09-05 02:01:16 +0200

    Add missing files

---
+++ The Great Mars Mystery/Assets/Scripts/EndCredits.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EndCredits : MonoBehaviour {
+    private void Start() {
+        SoundManager sm = FindObjectOfType<SoundManager>();
+        sm.StopPlayingSound("tune");
+        sm.Play("Theme");
+    }
+
+    public void ReloadToMainMenu() {
+        FindObjectOfType<GameManager>().GoBackToMainMenu();
+    }
+}
